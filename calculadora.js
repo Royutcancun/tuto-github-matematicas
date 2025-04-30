8a6dae7c31d2706db7a9fe33b3e1fc8406805f71
Rogelio Lopez <royutcancun@gmail.com>
2025-04-29 19:59:31 -0500

    Se agregan las funciones multiplicar y divir

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,12 @@ function sumar(a, b) {
 
   console.log(sumar(2,5));
   console.log(restar(12,5));
+
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
+  }
